465c1de9fa25f4bd7fbb4f05925f90e54328af2a
Mario Carvalho <mario.carvalho.devpython@gmail.com>
2024-11-23 10:47:45 -0300

    feat: creating chechToken and getUserById

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -1,122 +1,177 @@
-const createUserToken = require('../helpers/created-user-token')
-const User = require('../models/User')
-const bcrypt = require('bcrypt')
+const createUserToken = require("../helpers/created-user-token");
+const getToken = require('../helpers/get-token')
+const jwt = require("jsonwebtoken");
+const User = require("../models/User");
+const bcrypt = require("bcrypt");
 
 function isValidEmail(email) {
-    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
-    return regex.test(email)
+	const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+	return regex.test(email);
 }
 
 const register = async (req, res) => {
-    const { name, email, password, confirmPassword, image } = req.body
+	const { name, email, password, confirmpassword, image } = req.body;
 
-    if (!name) {
-        res.status(422).json({ message: "O nome é obrigatório!" })
+	if (!name) {
+		res.status(422).json({ message: "O nome é obrigatório!" });
+		return;
+	}
+
+	if (!email) {
+		res.status(422).json({ message: "O email é obrigatório!" });
+		return;
+	}
+
+	// Check is email is valid
+	if (!isValidEmail(email)) {
+		res.status(422).json({
+			message: "O email precisa ser um email valido!"
+		});
+		return;
+	}
+
+	if (!password) {
+		res.status(422).json({ message: "A senha é obrigatória!" });
+		return;
+	}
+
+	if (password.length < 6) {
+		res.status(422).json({
+			message: "A senha precisa ter no minimo 6 caracteres!"
+		});
+		return;
+	}
+
+	if (!confirmpassword) {
+		res.status(422).json({
+			message: "A confirmação de senha é obrigatória!"
+		});
+		return;
+	}
+
+    if(confirmpassword.length < 6) {
+        res.status(422).json({ message: "A confirmação de senha precisa ter no minimo 6 caracteres!"})
         return
     }
 
-    if (!email) {
-        res.status(422).json({ message: "O email é obrigatório!" })
+	if (confirmpassword !== password) {
+		res.status(422).json({
+			message: "A senha e a confirmação de senha náo são iguais!"
+		});
+		return;
+	}
+
+    if (!image) {
+        res.status(422).json({ message: "A imagem é obrigatória!" });
         return
     }
 
-    // Check is email is valid 
-    if (!isValidEmail(email)) {
-        res.status(422).json({ message: "O email precisa ser um email valido!" })
-        return
-    }
+	// Check if user exists
+	const userExists = await User.findOne({ email: email });
+	if (userExists) {
+		res.status(422).json({ message: "Por favor, utilize outro email!" });
+		return;
+	}
 
-    if (!password) {
-        res.status(422).json({ message: "A senha é obrigatória!" })
-        return
-    }
+	const salt = await bcrypt.genSalt(12);
+	const passwordHash = await bcrypt.hash(password, salt);
 
-    if (password.length < 6) {
-        res.status(422).json({ message: "A senha precisa ter no minimo 6 caracteres!" })
-        return
-    }
+	const user = new User({
+		name,
+		email,
+		password: passwordHash,
+		image,
+	});
 
-    if (!confirmPassword) {
-        res.status(422).json({ message: "A confirmação de senha é obrigatória!" })
-        return
-    }
-
-    if (confirmPassword !== password) {
-        res.status(422).json({ message: "A senha e a confirmação de senha náo são iguais!" })
-        return
-    }
-
-    // Check if user exists
-    const userExists = await User.findOne({ email: email })
-    if (userExists) {
-        res.status(422).json({ message: "Por favor, utilize outro email!" })
-        return
-    }
-
-    const salt = await bcrypt.genSalt(12)
-    const passwordHash = await bcrypt.hash(password, salt)
-
-    const user = new User({
-        name,
-        email,
-        password: passwordHash,
-        image
-    })
-
-    try {
-
-        const newUser = await user.save()
-        await createUserToken(newUser, req, res)
-        // await res.status(201).json({ message: `User created ${newUser.name}` })
-
-    } catch (error) {
-        await res.status(500).json({ error: error })
-    }
-}
+	try {
+		const newUser = await user.save();
+		await createUserToken(newUser, req, res);
+		// await res.status(201).json({ message: `User created ${newUser.name}` })
+	} catch (error) {
+		await res.status(500).json({ error: error });
+	}
+};
 
 const login = async (req, res) => {
-    const { email, password } = req.body
+	const { email, password } = req.body;
 
-    if (!email) {
-        res.status(422).json({ message: "O email é obrigatório!" })
-        return
-    }
+	if (!email) {
+		res.status(422).json({ message: "O email é obrigatório!" });
+		return;
+	}
 
-    if (!isValidEmail) {
-        res.status(422).json({ message: "O email precisa ser um email valido!" })
-        return
-    }
+	if (!isValidEmail) {
+		res.status(422).json({
+			message: "O email precisa ser um email valido!",
+		});
+		return;
+	}
 
-    if (!password) {
-        res.status(422).json({ message: "A senha é obrigatória!" })
-        return
-    }
+	if (!password) {
+		res.status(422).json({ message: "A senha é obrigatória!" });
+		return;
+	}
 
-    if (password.length < 6) {
-        res.status(422).json({ message: "A senha precisa ter no minimo 6 caracteres!" })
-        return
-    }
+	if (password.length < 6) {
+		res.status(422).json({
+			message: "A senha precisa ter no minimo 6 caracteres!",
+		});
+		return;
+	}
 
-    // Check if user exists - verifico se usuario existe
-    const user = await User.findOne({ email: email })
+	// Check if user exists - verifico se usuario existe
+	const user = await User.findOne({ email: email });
+	if (!user) {
+		res.status(422).json({
+			message: "Não há usuario cadastrado com esse email!",
+		});
+		return;
+	}
+
+	// Check if password match with db password
+	const checkPassword = await bcrypt.compare(password, user.password);
+
+	if (!checkPassword) {
+		res.status(422).json({ message: "Senha invalida!" });
+		return;
+	}
+
+	await createUserToken(user, req, res);
+};
+
+const checkUser = async (req, res) => {
+	let currentUser;
+
+	if (req.headers.authorization) {
+		const token = getToken(req);
+		const decoded = jwt.verify(token, "secret");
+
+		currentUser = await User.findById(decoded.id);
+
+		currentUser.password = undefined;
+	} else {
+		currentUser = null;
+	}
+
+	res.status(200).send(currentUser);
+};
+
+const getUserById = async (req, res) => {
+    const { id } = req.params
+
+    const user = await User.findById(id).select("-password")
+    
     if (!user) {
-        res.status(422).json({ message: "Não há usuario cadastrado com esse email!" })
+        res.status(422).json({message: "Usuario não encontrado!"})
         return
     }
 
-    // Check if password match with db password
-    const checkPassword = await bcrypt.compare(password, user.password)
-
-    if(!checkPassword) {
-        res.status(422).json({ message: "Senha invalida!"})
-        return
-    }
-
-    await createUserToken(user, req, res)
-
+    res.status(200).json({user})
 }
 
 module.exports = {
-    register,
-    login
-}
+	register,
+	login,
+	checkUser,
+    getUserById
+};
--- backend/helpers/created-user-token.js
+++ backend/helpers/created-user-token.js
@@ -1,17 +1,23 @@
-const jwt = require('jsonwebtoken')
+const jwt = require("jsonwebtoken");
 
 const createUserToken = (user, req, res) => {
-    // create token
-    const token = jwt.sign({
-        name: user.name,
-        id: user._id
-    }, "secret")
+	// create token
+	const token = jwt.sign(
+		{
+			name: user.name,
+			id: user._id,
+		},
+		"secret",
+		{
+			expiresIn: "1h",
+		}
+	);
 
-    res.status(200).json({
-        message: "Você está autenticado!",
-        token: token,
-        userId: user._id
-    })
-}
+	res.status(200).json({
+		message: "Você está autenticado!",
+		token: token,
+		userId: user._id,
+	});
+};
 
-module.exports = createUserToken
+module.exports = createUserToken;
